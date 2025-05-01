69a0cf78aa146d6d94241aeed2db91b18167a89e
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2025-05-01 18:20:18 +0500

    update: fix month calculation for left header button visibility in flutter_calendar_carousel

--- lib/flutter_calendar_carousel.dart
+++ lib/flutter_calendar_carousel.dart
@@ -357,7 +357,7 @@ class _CalendarState<T extends EventInterface>
               showLeftHeaderButtons:
                   widget.showHeaderButton && widget.weekFormat
                       ? true
-                      : (widget.minSelectedDate?.copyWith(day: 1,month: (widget.minSelectedDate?.month ?? 2) - 1).isBefore(
+                      : (widget.minSelectedDate?.copyWith(day: 1,month: (widget.minSelectedDate?.month ?? 2) + 1).isBefore(
                               this._dates[this._pageNum].copyWith(day: 1)) ??
                           true),
               showRightHeaderButtons:
